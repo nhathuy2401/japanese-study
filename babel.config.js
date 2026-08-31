16790c93f2ac3857d4afac61d1c1ba31bff658d9
huy.tran@codecomplete.jp <huy.tran@codecomplete.jp>
2026-08-31 17:07:52 +0700

    fix: resolve Babel preset and react-native-worklets configuration for Reanimated v4

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      ['@babel/plugin-proposal-decorators', { legacy: true }],
-      ['@babel/plugin-proposal-class-properties', { loose: true }],
-      'react-native-reanimated/plugin',
-    ],
   };
 };
-
